0d3fb3da5c08c9437877520f72db4f9def4b8ffe
Thiago Borges Abdnur <bolaum@gmail.com>
2017-08-31 15:10:18 -0300

    Fix fix fix

--- install_base.sh
+++ install_base.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-APT_BASE_PKGS="awk vim tmux build-essential curl wget git subversion samba"
+APT_BASE_PKGS="vim tmux build-essential curl wget git subversion samba"
 PYTHON_BUILD_PKGS="libncursesw5-dev libreadline-dev libssl-dev libgdbm-dev libc6-dev libsqlite3-dev tk-dev libbz2-dev"
 # packages to be instaled
 ALL_PKGS="$APT_BASE_PKGS $PYTHON_BUILD_PKGS"
+
+# config files to copy
+CONFIG_FILES=".bashrc .gitconfig .tmux.conf.local .vimrc"
 
 # python versions to be installed
 PYTHON_VERSIONS="2.7.13 3.6.2"
@@ -11,28 +14,25 @@ PYTHON_VERSIONS="2.7.13 3.6.2"
 # ruby versions to install
 RUBY_VERSIONS="2.4.1"
 
-# exit on error
-set -e
-
 # clone bash_colors
 echo "Clonning .bash_colors..."
-git clone https://github.com/mercurio21/bash_colors.git .bash_colors
-source .bash_colors/bash_colors.sh
+git clone https://github.com/mercurio21/bash_colors.git $HOME/.bash_colors
+source $HOME/.bash_colors/bash_colors.sh
 
 # update and install base packages
 clr_bold clr_cyan "Installing base packages..."
 sudo apt-get update
-sudo apt-get install $ALL_PKGS
+sudo apt-get upgrade
+sudo apt-get install -y $ALL_PKGS
 
 # clone bash-it
 clr_bold clr_cyan "Clonning bash-it..."
-git clone https://github.com/Bash-it/bash-it.git .bash_it
-ln -s bash-it-themes/ .bash_it/custom/themes
+git clone --depth=1 https://github.com/Bash-it/bash-it.git $HOME/.bash_it
 
 # clone .tmux
 clr_bold clr_cyan "Clonning .tmux..."
-git clone https://github.com/gpakosz/.tmux.git .tmux
-ln -s -f .tmux/.tmux.conf
+git clone https://github.com/gpakosz/.tmux.git $HOME/.tmux
+ln -s -f $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
 
 # install pyenv
 clr_bold clr_cyan "Installing pyenv..."
@@ -48,9 +48,18 @@ clr_bold clr_cyan "Installing rvm..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable
 
-# get bash configs running
+# install bash-it
+$HOME/.bash_it/install.sh -s
 clr_bold clr_cyan "Sourcing .bashrc..."
-source .bashrc
+source $HOME/.bash_rc
+bash-it show
+
+# copy config files
+cp -a bash-it-themes $HOME/.bash-it-themes || true
+echo ln -s $HOME/.bash-it-themes $HOME/.bash_it/custom/themes
+ln -s $HOME/.bash-it-themes $HOME/.bash_it/custom/themes
+cp -a bin/ $HOME || true
+cp -a $CONFIG_FILES $HOME
 
 # enable bash-it plugins
 clr_bold clr_cyan "Enabling bash-it plugins..."
@@ -58,11 +67,11 @@ bash-it enable plugin base alias-completion tmux git subversion pyenv node rvm r
 
 # create default dirs
 clr_bold clr_cyan "Creating default dirs..."
-mkdir -p tmp projs downloads
+mkdir -p $HOME/{tmp,projs,downloads}
 
 # install python, set first version as default and install basic packages
 clr_bold clr_cyan "Installing python..."
-echo $PYTHON_VERSIONS | xargs -n 1 pyenv install -kv
+echo $PYTHON_VERSIONS | xargs -n 1 pyenv install -k
 pyenv global $(echo $PYTHON_VERSIONS | awk '{print $1;}')
 pip install ipython
 
@@ -72,36 +81,40 @@ echo $RUBY_VERSIONS | xargs -n 1 rvm install
 rvm --default use $(echo $RUBY_VERSIONS | awk '{print $1;}')
 
 # download fonts
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.otf?raw=true -O tmp/FuraCodeNFRegularWindows.otf
+clr_bold clr_cyan "Downloading fonts..."
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.otf?raw=true -O $HOME/tmp/FuraCodeNFRegularWindows.otf
 
-wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.otf?raw=true -O tmp/FuraCodeNFRegularMono.otf
+wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fura%20Code%20Regular%20Nerd%20Font%20Complete%20Mono.otf?raw=true -O $HOME/tmp/FuraCodeNFRegularMono.otf
 
 # show a list of manual steps
 END_MESSAGE="
 Instalation done!
 Please do the following manually:
-  - Install nerd fonts in tmp/ to terminal emulator
-  - Add ssh key to github
+\t- Install nerd fonts in tmp/ to terminal emulator
+\t- Add ssh key to github
 
 For VMs:
-    - Add samba password
-      $ smbpasswd
-    - Add the following to '/etc/samba/smb.conf' to share home via samba
+\t- Set samba password
+\t\t$ smbpasswd
+\t- Add the following to '/etc/samba/smb.conf' to share home via samba
 
 [vmhome]
-    path = $HOME
-    valid users = $USER
-    read only = no
-    create mask = 664
-    directory mask = 2755
-    map archive = no
+\tpath = $HOME
+\tvalid users = $USER
+\tread only = no
+\tcreate mask = 664
+\tdirectory mask = 2755
+\tmap archive = no
 
-    - restart samba:
-      $ sudo service smbd restart
+\t- restart samba:
+\t\t$ sudo service smbd restart
 
 These will test for high color support in terminal:
 "
-clr_bold clr_cyan $END_MESSAGE
+while read -r line; do
+  clr_escape "$line" $CLR_BOLD $CLR_CYAN
+done <<< "$END_MESSAGE"
+
 24-bit-color.sh
 echo
 testtruecolor.sh
